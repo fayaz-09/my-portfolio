a1338ae5bf75e83631154c168df2fcf9b6d77292
fayaz-09 <fayazseedat09@gmail.com>
2024-03-11 15:10:16 +0000

    added sidebar and began work on home page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,40 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import LogoF from '../../assets/images/Logo1.png'
+import LogoSub from '../../assets/images/logoSub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            
-        </Link>
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+            <img src={LogoF} alt="logo"/>   
+            <img className="sub-logo" src={LogoSub} alt="fayaz"/>
+        </Link> 
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="www.linkedin.com/in/fayaz-seedat-220b72149">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://github.com/fayaz-09">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
